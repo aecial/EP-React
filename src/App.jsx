487850786a821aa3a3ae07fafd185da17b90ae99
Teddy Pascual <teddylayson2000@gmail.com>
2023-09-09 19:42:13 +0800

    rebuild from hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-import Menu from "./components/Menu";
-import Footer from "./components/Footer";
-import Contact from "./components/Contact";
 function App() {
   return (
     <>
       <Navbar />
       <Hero imgSrc={"./src/assets/bird.png"} heroText={"The BEST Fried Itik"} />
-      <Menu />
-      <Contact />
-      <Footer />
     </>
   );
 }
--- src/components/ContactBody.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ContactBody = ({ content }) => {
-  return <div className="w-full">{content}</div>;
-};
-
-export default ContactBody;
