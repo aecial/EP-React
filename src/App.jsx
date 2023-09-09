f2965d406a20f4ae0d372015f9d57a106c9d7eb5
Teddy Pascual <teddylayson2000@gmail.com>
2023-09-09 20:00:33 +0800

    new concept menu bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Menu from "./Menu";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 function App() {
@@ -5,6 +6,7 @@ function App() {
     <>
       <Navbar />
       <Hero imgSrc={"./src/assets/bird.png"} heroText={"The BEST Fried Itik"} />
+      <Menu />
     </>
   );
 }
---
+++ src/Menu.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Menu = () => {
+  return (
+    <div className="h-[100vh] bg-black text-white">
+      <div className="absolute bg-black border-r-4 border-white h-full w-[30%] flex flex-col items-center justify-center gap-4 p-3">
+        <button className="bg-white text-black p-2 w-full rounded-2xl">
+          Fried Itik
+        </button>
+        <button className="bg-white text-black p-2 w-full rounded-2xl">
+          Fried Itik
+        </button>
+        <button className="bg-white text-black p-2 w-full rounded-2xl">
+          Fried Itik
+        </button>
+      </div>
+      <div className="text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, illo
+        magni? Nam inventore sequi expedita reiciendis ducimus quas repudiandae
+        fugit cupiditate impedit minima! Quae voluptates, ab magni eum deleniti
+        necessitatibus.
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
