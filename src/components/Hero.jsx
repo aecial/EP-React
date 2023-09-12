5e91f7478e9bb16649e4feec786c66ea072157b9
Teddy Pascual <teddylayson2000@gmail.com>
2023-09-12 19:42:35 +0800

    enlarge hero pic

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Hero = ({ imgSrc, heroText }) => {
   return (
     <div className="hero-height gap-4 bg-slate-100 flex justify-center p-4 items-center flex-col">
-      <img src={imgSrc} className="w-[100%] md:w-[30%]" />
+      <img src={imgSrc} className="w-[100%] md:w-[50%]" />
       <h1 className="text-4xl md:text-5xl">{heroText}</h1>
     </div>
   );
